92a9ad7923814bcd0c217b758bdea1ab7cf1403f
Larry Shields <larry.shields@gmail.com>
2025-06-15 18:25:37 -0400

    Added WNBA Stats Retrieval

---
+++ jobs/factories.py
@@ -0,0 +1,122 @@
+"""
+Airflow Factories
+"""
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python import PythonOperator
+from airflow.providers.cncf.kubernetes.operators.job import KubernetesJobOperator
+
+import helpers
+
+
+class DagFactory:
+    """
+    Factory to generate and provide Common DAG Instances
+    """
+
+    @staticmethod
+    def create_schedule_dag(*, secret_name: str | None = None, identifier: str | None = None,
+                            tags: list | None = None,
+                            bucket: str | None = None,
+                            url: str | None = None,
+                            parameters:dict) -> DAG:
+        """
+        Creates a Schedule DAG
+        :param secret_name: Name of Kubernetes Job Secret
+        :param identifier: Dag identifier
+        :param tags: Dag Tags
+        :param bucket: Bucket Variable Name
+        :param url: Base URL Variable Name
+        :param parameters: Dag Parameters
+        :return: DAG
+        """
+        if tags is None:
+            tags = []
+
+
+        with DAG(dag_id=f"{identifier}-schedule-dag", schedule=None, catchup=False, tags=tags,
+                 params=parameters,
+                 default_args={'provider_context': True}) as schedule_dag:
+
+            commands = [
+                'python',
+                'schedule_puller.py',
+                '-b',
+                Variable.get(bucket)]
+
+            args = {
+                'secret_name': secret_name,
+                'job_name': f"{identifier}-schedule-job",
+                'base_url': url,
+                'commands': commands,
+                'type': 'schedule'
+            }
+
+            template = PythonOperator(task_id='template-generator',
+                                      python_callable=helpers.publish_template, op_kwargs=args)
+
+            k8_job = KubernetesJobOperator(task_id=f"{identifier}-schedule-job",
+                                           job_template_file="{{ ti.xcom_pull(task_ids='template-generator', key='job-file') }}",
+                                           backoff_limit=5,
+                                           wait_until_job_complete=True, job_poll_interval=60,
+                                           ttl_seconds_after_finished=300)
+
+            clean_up = PythonOperator(task_id='clean-up-template',
+                                      python_callable=helpers.clean_up_templates)
+
+            template >> k8_job >> clean_up
+
+        return schedule_dag
+
+    @staticmethod
+    def create_stats_dag(*, secret_name: str | None = None,
+                         identifier: str | None = None,
+                         tags: list | None = None,
+                         bucket:str | None = None,
+                         url:str | None = None,
+                         parameters: dict) -> DAG:
+        """
+        Creates a DAG for retrieving Statistics
+        :param secret_name: Variable for the Secret Name
+        :param identifier: Dag identifier
+        :param tags: Tags
+        :param bucket: Bucket Variable Name
+        :param url: Base Url variable name
+        :param parameters: DAG Parameters
+        :return:
+        """
+
+
+        if not tags:
+            tags = []
+
+
+        with DAG(dag_id=f"{identifier}-stat-dag", schedule=None, catchup=False, tags=tags,
+                 params=parameters,
+                 default_args={'provider_context': True}) as stat_dag:
+
+            commands = ['python', 'stats_puller.py', '-b', Variable.get(bucket)]
+
+            args = {
+                'secret_name': secret_name,
+                'job_name': f"{identifier}-stats-job",
+                'base_url': url,
+                'commands': commands,
+                'type': 'stats'
+            }
+
+            template = PythonOperator(task_id='template-generator',
+                                      python_callable=helpers.publish_template, op_kwargs=args)
+
+            k8_job = KubernetesJobOperator(task_id=f"{identifier}-stats-job",
+                                           job_template_file="{{ ti.xcom_pull(task_ids='template-generator', key='job-file') }}",
+                                           backoff_limit=5,
+                                           wait_until_job_complete=True, job_poll_interval=60,
+                                           ttl_seconds_after_finished=300)
+
+            clean_up = PythonOperator(task_id='clean-up-template',
+                                      python_callable=helpers.clean_up_templates)
+
+            template >> k8_job >> clean_up
+        return stat_dag
--- jobs/helpers.py
+++ jobs/helpers.py
@@ -1,9 +1,16 @@
 """
 Helper Functions for the application
 """
+import json
+import logging
+import os
+import uuid
+
+from airflow.models import Variable
 
 
-def create_job_spec(secret_name: str, job_name: str, image: str, commands: list[str], base_url:str) -> dict:
+def create_job_spec(secret_name: str, job_name: str, image: str, commands: list[str],
+                    base_url: str) -> dict:
     """
     Job Template Spec Creation
     :param secret_name: Pod Secret Name
@@ -83,3 +90,70 @@ def create_job_spec(secret_name: str, job_name: str, image: str, commands: list[
         }
 
     }
+
+
+def publish_template(**kwargs):
+    """
+    Publishes the Job Template to the Temp Directory
+    """
+
+    logger = logging.getLogger(__name__)
+    logger.info('Building Template')
+
+    task_instance = kwargs['ti']
+    params = kwargs['params']
+    root = params['temp']
+
+    commands:list = kwargs['commands']
+
+    if 'date' in params:
+        commands.append('-d')
+        commands.append(params['date'])
+
+    if 'group' in params:
+        commands.append('-g')
+        commands.append(params['group'])
+
+    if 'schedule' in params:
+        commands.append('-s')
+        commands.append(params['schedule'])
+
+    if 'week' in params:
+        commands.append('-w')
+        commands.append(params['week'])
+
+    if 'year' in params:
+        commands.append('-y')
+        commands.append(params['year'])
+
+    if 'season' in params:
+        commands.append('-s')
+        commands.append(params['season'])
+
+    job_spec = create_job_spec(Variable.get(kwargs['secret_name']), kwargs['job_name'],
+                               Variable.get('STAT_IMAGE'),
+                               commands,
+                               Variable.get(kwargs['base_url']))
+
+    output_path = os.path.join(root, 'templates')
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    file_name = f"{uuid.uuid4()}-{kwargs['job_name']}-{kwargs['type']}-job.json"
+    with open(os.path.join(output_path, file_name), 'w') as f:
+        json.dump(job_spec, f)
+
+    task_instance.xcom_push(key='job-file',
+                            value=os.path.join(output_path, file_name))
+
+
+def clean_up_templates(**kwargs):
+    """
+    Cleans up the Temp Directory
+    """
+
+    task_instance = kwargs['ti']
+    path = task_instance.xcom_pull(task_ids='template-generator', key='job-file')
+    if os.path.exists(path):
+        os.remove(path)
